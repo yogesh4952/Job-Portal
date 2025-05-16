16f0c2206716418797c0553e4cbafa6861795007
yogesh4952 <yogeshshah2063@gmail.com>
2025-05-16 14:13:51 +0545

    Add recruiterLogin.jsx

--- client/src/Components/RecruiterLogin.jsx
+++ client/src/Components/RecruiterLogin.jsx
@@ -1,4 +1,4 @@
-import React, { startTransition, useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { assets } from "../assets/assets";
 import { AppContext } from "../context/AppContext";
 import axios from "axios";
